69e12419185e60109c28019cfca871a3ff90910e
dfneto <pdavidfneto@gmail.com>
2024-03-09 16:45:08 +0100

    Enviei por render um dicionário e usei este para preencher as páginas dinamicamente. Tbm apliquei filtros para formatar as páginas

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,11 @@ from datetime import date
 
 from django.shortcuts import render
 
-
 all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Maximilian",
+        "author": "dragon",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
@@ -28,7 +27,7 @@ all_posts = [
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
-        "author": "Maximilian",
+        "author": "d",
         "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
@@ -49,7 +48,7 @@ all_posts = [
     {
         "slug": "into-the-woods",
         "image": "woods.jpg",
-        "author": "Maximilian",
+        "author": "vi",
         "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
@@ -74,14 +73,19 @@ def get_date(post):
 
 # Create your views here.
 def starting_page(request):
-  sorted_posts = sorted(all_posts, key=get_date)
-  latest_posts = sorted_posts[-3:]
-  return render(request, "blog/index.html", {
-    "posts" : latest_posts
-  })
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+      "posts": latest_posts
+    })
 
 def posts(request):
-  return render(request, "blog/all-posts.html")
+  return render(request, "blog/all-posts.html", {
+    "all_posts" : all_posts
+  })
 
 def post_detail(request, my_slug):
-  return render(request, "blog/post-detail.html")
+  post_returned = next(post for post in all_posts if post['slug'] == my_slug)
+  return render(request, "blog/post-detail.html", {
+     "post" : post_returned
+  })
